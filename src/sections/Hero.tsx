3528bb7f3ac8a81681ef88615366c491be79f568
TManhique <manhiquet0@gmail.com>
2025-02-11 13:08:09 +0200

    Hero stats style completed

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,14 +1,18 @@
 export default function Hero() {
     return (
-        <section>
+        <section className="py-24">
             <div className="container">
-                <div>$7.5M seed round raised</div>
+                <div className="flex justify-center">
+                    <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold">
+                        🌟 $7.5M seed round raised
+                    </div>
+                </div>
                 <h1>Impactful design, created effortlessly</h1>
 
                 <p>
-                    Design tools shouldn&apos;t slow down. Layers combines powerful
-                    features with an intuitive interface that keseps you in your
-                    creative flow.
+                    Design tools shouldn&apos;t slow down. Layers combines
+                    powerful features with an intuitive interface that keseps
+                    you in your creative flow.
                 </p>
 
                 <form>
