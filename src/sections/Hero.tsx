5b4957728da307050684332ae0a843f97297c69b
TManhique <manhiquet0@gmail.com>
2025-02-11 12:31:10 +0200

    Hero content added

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,21 @@
 export default function Hero() {
-    return <div>Hero</div>;
+    return (
+        <section>
+            <div className="container">
+                <div>$7.5M seed round raised</div>
+                <h1>Impactful design, created effortlessly</h1>
+
+                <p>
+                    Design tools shouldn&apos;t slow down. Layers combines powerful
+                    features with an intuitive interface that keseps you in your
+                    creative flow.
+                </p>
+
+                <form>
+                    <input type="email" placeholder="Enter your email" />
+                    <button type="submit">Sign Up</button>
+                </form>
+            </div>
+        </section>
+    );
 }
