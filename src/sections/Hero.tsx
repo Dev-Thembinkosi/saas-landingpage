8d6dd0184b01b6ca61cf6ebdc3c4e321fa206b3a
TManhique <manhiquet0@gmail.com>
2025-03-19 21:02:54 +0200

    logo ticker complete

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,34 +1,59 @@
 import Button from "@/components/Button";
+import designExample1Image from "@/assets/images/design-example-1.png";
+import designEaxmple2Image from "@/assets/images/design-example-2.png";
+import Image from "next/image";
+import Pointer from "@/components/Pointer";
+
 
 export default function Hero() {
     return (
-        <section className="py-24">
-            <div className="container">
+        <section className="py-24 overflow-x-clip">
+            <div className="container relative">
+                <div className="absolute -left-32 top-16 hidden lg:block">
+                    <Image
+                        src={designExample1Image}
+                        alt="design example 1 image"
+                    />
+                </div>
+
+                <div className="absolute -right-64 -top-16 hidden lg:block">
+                    <Image
+                        src={designEaxmple2Image}
+                        alt="Design example 2 image"
+                    />
+                </div>
+                <div className="absolute left-56 top-96 hidden lg:block">
+                    <Pointer name="Andrea"/>
+                </div>
+
+                <div className="absolute right-80 -top-4 hidden lg:block">
+                    <Pointer name="James" color="red"/>
+                </div>
                 <div className="flex justify-center">
                     <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold">
-                        🌟 $7.5M seed round raised
+                        ✨ $7.5M seed round raised
                     </div>
                 </div>
 
-                <h1 className="text-6xl font-medium text-center mt-6">
+                <h1 className="text-6xl md:text-7xl lg:text-8xl font-medium text-center mt-6">
                     Impactful design, created effortlessly
                 </h1>
-                <p className="text-center text-xl text-white/50 mt-8">
-                    Design tools shouldn&apos;t slow down. Layers combines
-                    powerful features with an intuitive interface that keseps
-                    you in your creative flow.
+                <p className="text-center text-xl text-white/50 mt-8 max-w-2xl mx-auto">
+                    Design tools shouldn&apos;t slow down. Layers combined
+                    powerful features with an intuitive interface that keeps you
+                    in your creative flow.
                 </p>
 
-                <form className="flex  border border-white/15 rounded-full p-2 mt-8">
+                <form className="flex border border-white/15 rounded-full p-2 mt-8 max-w-lg mx-auto">
                     <input
                         type="email"
                         placeholder="Enter your email"
-                        className="bg-transparent px-4"
+                        className="bg-transparent px-4 md:flex-1 min-w-0 w-full"
                     />
                     <Button
                         type="submit"
                         variant="primary"
-                        className="whitespace-nowrap"
+                        className="w-auto whitespace-nowrap px-4"
                         size="sm"
                     >
                         Sign Up
