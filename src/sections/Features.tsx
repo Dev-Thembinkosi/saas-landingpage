3e622a2e62f134d047e5b9dacf4847f3f443d487
TManhique <manhiquet0@gmail.com>
2025-03-21 06:15:55 +0200

    Feature 1 complete

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -1,3 +1,11 @@
+import FeatureCard from "@/components/FeatureCard";
+import Tag from "@/components/Tag";
+import Avatar1 from "@/assets/images/avatar-ashwin-santiago.jpg";
+import Avatar2 from "@/assets/images/avatar-lula-meyers.jpg";
+import Avatar3 from "@/assets/images/avatar-florence-shaw.jpg";
+import Image from "next/image";
+import Avatar from "@/components/Avatar";
+
 const features = [
     "Asset Library",
     "Code Preview",
@@ -9,5 +17,78 @@ const features = [
 ];
 
 export default function Features() {
-    return <div>Features</div>;
+    return (
+        <section className="py-24">
+            <div className="container">
+                <div className="flex justify-center">
+                    <Tag>Features</Tag>
+                </div>
+                <h2 className="text-6xl font-medium text-center mt-6">
+                    Where power meets{" "}
+                    <span className="text-lime-400">Simplicity</span>{" "}
+                </h2>
+                <div className="mt-12 grid grid-cols-1 grap-8">
+                    <FeatureCard
+                        title="Real-time Collaboration"
+                        description="Work together seamlessly with conflict-free team
+                                editing"
+                    >
+                        <div className="aspect-video flex items-center justify-center">
+                            <Avatar className="z-40">
+                                <Image
+                                    src={Avatar1}
+                                    alt="avatar 1"
+                                    className="rounded-full"
+                                />
+                            </Avatar>
+                            <Avatar className="-ml-6 border-indigo-500 z-30">
+                                <Image
+                                    src={Avatar2}
+                                    alt="Avatar 2"
+                                    className="rounded-full"
+                                />
+                            </Avatar>
+                            <Avatar className="-ml-6 border-amber-500 z-20">
+                                <Image
+                                    src={Avatar3}
+                                    alt="Avatar 3"
+                                    className="rounded-full"
+                                />
+                            </Avatar>
+                            <Avatar className="-ml-6 border-transparent">
+                                <div className="size-full bg-neutral-700 rounded-full inline-flex items-center justify-center gap-1">
+
+                                    {Array.from({length:3}).map((_,i) =>(
+                                        <span key={i} className="size-1.5 rounded-full bg-white inline-flex"></span>
+                                    ))}
+
+                                    
+                                </div>
+                            </Avatar>
+                        </div>
+                    </FeatureCard>
+                    <FeatureCard
+                        title="Interactive Prototyping"
+                        description="Engage your clients with Prototyping that react
+                                to user actions"
+                    ></FeatureCard>
+
+                    <FeatureCard
+                        title="Keyboard Quick Actions"
+                        description=" Powerfull commamds to help you create desings
+                                more quickly"
+                    ></FeatureCard>
+                </div>
+
+                <div>
+                    {features.map((feature) => (
+                        <div key={feature}>
+                            <span></span>
+                            <span>{feature}</span>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
 }
--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -23,7 +23,9 @@ export default function LogoTicker() {
     return (
         <section className="py-24 overflow-x-clip">
             <div className="container">
-                <h3 className="text-center text-white/50 text-xl">Already chosen by these market leaders</h3>
+                <h3 className="text-center text-white/50 text-xl">
+                    Already chosen by these market leaders
+                </h3>
                 <div className="overflow-hidden mt-12 [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
                     <div className="flex gap-24 pr-24">
                         {logos.map((logo) => (
